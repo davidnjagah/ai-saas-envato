107e4324fe410179230c9a52076f4c94ee6d16ad
David Washi <dnjagah@gmail.com>
2024-01-07 21:36:41 +0300

    Editing dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -51,8 +51,14 @@ const tools = [
 ]
 
 
-export default function DashboardPage() {
+export default function DashboardPage({ 
+  params, searchParams
+} : {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
   const router = useRouter();
+  console.log(searchParams);
   return (
     <div>
     <div className="mb-8 space-y-4">
@@ -66,7 +72,7 @@ export default function DashboardPage() {
     <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
             <Card
-            onClick={() => router.push(tool.href)}
+            onClick={() => router.push(tool.href+`?v=${searchParams.v}`)}
             key={tool.href}
             className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
               <div className="flex items-center gap-x-4">
